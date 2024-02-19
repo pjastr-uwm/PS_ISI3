e6bded3dbd01637d198fdc87a23a30752229f7d6
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-19 14:23:57 +0100

    lav1

---
+++ lab1/zad8-10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("To jest cytat: \"Czesto uzywam jezyka C.\"\n");
+    printf("C:\\Program Files\\MojaAplikacja\\\n");
+    printf("C:\\\\Program Files\\\\MojaAplikacja\\\\\n");
+    printf("Specjalne znaki: \\t (tabulacja), \\n (nowa linia), %% (procent), \\ (ukoœnik wsteczny).");
+    return 0;
+}
