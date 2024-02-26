f35efe85979555a6549b27f5b2e47e9d340f557c
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-26 13:41:56 +0100

    petle

---
+++ lab2/petle/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0;
+    while (i < 10) {
+        printf("%d\n", i);
+        i++;
+    }
+    int j;
+    for(; j<5;j++)
+    {
+        printf("%d\n", j);
+    }
+    return 0;
+}
