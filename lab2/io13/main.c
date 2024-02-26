ec3d8f4b2836a8ea359779793426cb2ea26be8c3
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-26 12:36:31 +0100

    uzupelnienie

---
+++ lab2/io13/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float a;
+    scanf("%f", &a);
+    printf("%f", fabs(a));
+    return 0;
+}
---
+++ lab2/io4/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    float srednia = (a+b+c)/3.0;
+    printf("%f", srednia);
+    return 0;
+}
