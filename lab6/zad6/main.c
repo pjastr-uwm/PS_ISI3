7a5aa4e020f2cf5b52b4c1efecafee5f30175611
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-03-25 13:44:23 +0100

    zad6

---
+++ lab6/zad6/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverseArr(int n, int *tab){
+    for(int i=0;i<n/2;i++){
+        int temp = *(tab+i);
+        *(tab+i) = *(tab+n-1-i);
+        *(tab+n-1-i) = temp;
+    }
+}
+
+void printTable(int n, int *tab){
+    for(int i=0;i<n;i++){
+        printf("%d ", *(tab+i));
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int * tab1 = malloc(5*sizeof(int));
+    *tab1 = 4;
+    *(tab1+1) = -3;
+    *(tab1+2) = 11;
+    *(tab1+3) = 18;
+    *(tab1+4) = -25;
+    printTable(5, tab1);
+    reverseArr(5, tab1);
+    printTable(5, tab1);
+    return 0;
+}
