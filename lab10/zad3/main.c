1ecb9f15db34409a023363df7dc1534ecfb0161b
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-04-29 13:17:37 +0200

    zad3

---
+++ lab10/zad3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxRowIdx(int n, int m, int tab[n][m]){
+    int max = tab[0][0];
+    int max_index=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if (tab[i][j] > max){
+                max = tab[i][j];
+                max_index=i;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int tab[4][3] = {{3,-4,2},{1,-8,9},{33,2,4},{8,22,33}};
+    printf("%d\n", maxRowIdx(4,3,tab));
+    return 0;
+}
